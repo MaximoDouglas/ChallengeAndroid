7d55656ecc56fee49e603ffe2ed0155bc972aca6
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-11 02:16:02 -0300

    Add fake repository data to manually test the component

--- ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/app/modules/selectrepository/SelectRepositoryFragment.kt
+++ ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/app/modules/selectrepository/SelectRepositoryFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import br.com.argmax.challengeandroid.R
+import br.com.argmax.challengeandroid.app.components.dto.RepositoryCardDto
 import br.com.argmax.challengeandroid.databinding.SelectRepositoryFragmentBinding
 
 class SelectRepositoryFragment : Fragment() {
@@ -26,6 +27,30 @@ class SelectRepositoryFragment : Fragment() {
         )
 
         return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRepositoryCard()
+    }
+
+    private fun setupRepositoryCard() {
+        val fakeRepositoryCardDto =
+            RepositoryCardDto(
+                repositoryName = "Nome repositório",
+                repositoryDescription = "Lorem Ipsum is simply dummy text of the printing and " +
+                        "typesetting industry. Lorem Ipsum has been the industry's standard dummy " +
+                        "text ever since the 1500s, when an unknown printer took a galley of type " +
+                        "and scrambled it to make a type specimen book. It has",
+                forkQuantity = 640.toString(),
+                starsQuantity = 98.toString(),
+                userImageUrl = "https://picsum.photos/200",
+                userName = "username",
+                userFullName = "Nome Sobrenome"
+            )
+
+        mBinding?.fakeRepositoryCardComponent?.setRepositoryCardDto(fakeRepositoryCardDto)
     }
 
 }
