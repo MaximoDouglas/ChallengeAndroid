080d8ca8a8656a293641f30c7d66b881b0842b55
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-11 12:14:44 -0300

    Update string compactor util

--- ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/utils/stringutils/StringUtils.kt
+++ ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/utils/stringutils/StringUtils.kt
@@ -1,5 +1,9 @@
 package br.com.argmax.challengeandroid.utils.stringutils
 
 fun compactStringWithDots(string: String, maxLength: Int): String {
-    return string.subSequence(0, maxLength) as String + "..."
+    if (string.length > maxLength) {
+        return string.subSequence(0, maxLength) as String + "..."
+    }
+
+    return string
 }
