b873c5614bb392cdcaa80d75825646669cd2b410
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-11 02:15:27 -0300

    Add extension function to put an image in a rounded imageview

--- ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/app/extensions/ImageViewBindingAdapter.kt
+++ ChallengeAndroid/app/src/main/java/br/com/argmax/challengeandroid/app/extensions/ImageViewBindingAdapter.kt
@@ -1,4 +1,22 @@
-package br.com.argmax.challengeandroid.app.viewutils
+package br.com.argmax.challengeandroid.app.extensions
 
-class ImageViewBindingAdapter {
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import br.com.argmax.challengeandroid.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+
+object ImageViewBindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter(value = ["imageUri"])
+    fun ImageView.setImageUri(imageUri: String? = "") {
+        Glide.with(context)
+            .load(imageUri)
+            .placeholder(R.drawable.ic_user)
+            .error(R.drawable.ic_user)
+            .transform(CircleCrop())
+            .into(this)
+    }
+
 }
